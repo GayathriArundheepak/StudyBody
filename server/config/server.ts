ab01e38e7c64001d4218d231e38a535421595d07
GayathriArundheepak <gayathriravikumar18@gmail.com>
2024-05-27 09:58:08 +0530

    correct the error in server.ts  in dirname

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,29 +1,36 @@
 import express from "express";
 import cors from 'cors';
-import dotenv from 'dotenv'
-const cookieParser = require('cookie-parser');
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+dotenv.config();
 
 // Define __filename and __dirname for ES6 modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config()
+const createServer = () => {
+    const app = express();
+    
+    // Serve static files from the 'public' directory
+    app.use(express.static(path.join(__dirname, '../public')));
 
-const createServer=()=>{
-const app=express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-     // Use cookie-parser middleware
-      app.use(cookieParser());
-     // Enable CORS middleware
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    credentials: true // Allow credentials (cookies)
-  })
-  );
-    return app
+    // Use middleware to parse JSON bodies
+    app.use(express.json());
+
+    // Use cookie-parser middleware
+    app.use(cookieParser());
+
+    // Enable CORS middleware
+    app.use(cors({
+        origin: 'http://localhost:3000', // Allow requests from this origin
+        credentials: true // Allow credentials (cookies)
+    }));
+
+    return app;
 }
+
 export default createServer;
