153e85f802e6843a1a537fcd8cabfcd30e65b40b
GayathriArundheepak <gayathriravikumar18@gmail.com>
2024-05-27 08:19:44 +0530

    correct the error in server.ts  in dirname

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,20 +1,21 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv'
-import multer from 'multer';
 const cookieParser = require('cookie-parser');
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Define __filename and __dirname for ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config()
 
-
 const createServer=()=>{
-    
-    const app=express()
-
+const app=express();
 app.use(express.static(path.join(__dirname, 'public')));
-
-     app.use(express.json());
-
+app.use(express.json());
      // Use cookie-parser middleware
       app.use(cookieParser());
      // Enable CORS middleware
@@ -23,9 +24,6 @@ app.use(cors({
     credentials: true // Allow credentials (cookies)
   })
   );
-
-
-
     return app
 }
 export default createServer;
