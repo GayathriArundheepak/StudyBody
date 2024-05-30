771c3958121fba74605cf68ce575e441efcc8992
GayathriArundheepak <gayathriravikumar18@gmail.com>
2024-05-30 19:41:07 +0530

    client modified

--- client/src/components/PrivateRoutes/StudentRoute.tsx
+++ client/src/components/PrivateRoutes/StudentRoute.tsx
@@ -11,7 +11,6 @@ const StudentRoute: React.FC = () => {
   // Use RootState interface with useSelector hook
   const { userType } = useSelector((state: RootState) => state.user);
   
-
   // Return the appropriate JSX based on user type
   return userType === 'student' ? <Outlet /> : <Navigate to="/signin" />;
 };
