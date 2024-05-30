b503dfc0329b49ccc1ede053e01f6992fe751b43
GayathriArundheepak <gayathriravikumar18@gmail.com>
2024-05-30 23:00:02 +0530

    modified  messenger

--- client/src/pages/messanger/Messanger.tsx
+++ client/src/pages/messanger/Messanger.tsx
@@ -37,10 +37,8 @@ const Messanger: React.FC = () => {
   const scrollRef = useRef<HTMLDivElement | null>(null);
   console.log("socket", socket);
   useEffect(() => {
-    // socket.current = io("ws://localhost:8900");
     socket.current = io("ws:43.205.115.45:8900");
     socket.current.on("getMessage", (data) => {
-      console.log("data:", data);
       if (currentChat?.isGroup) {
         setArrivalMessage({
           conversationId: data.conversationId || "",
@@ -65,7 +63,7 @@ const Messanger: React.FC = () => {
         socket.current = null;
       }
     };
-  }, [currentUser]);
+  },[currentUser]);
   useEffect(() => {
     arrivalMessage &&
       currentChat?.members.includes(arrivalMessage.sender) &&
