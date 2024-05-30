c5eb603370df793ed6d437bc7e48bf1b7f55f5b2
GayathriArundheepak <gayathriravikumar18@gmail.com>
2024-05-30 23:40:40 +0530

    updated home and teacherpage

--- client/src/pages/course/updateCourses/TeacherCoursesPage.tsx
+++ client/src/pages/course/updateCourses/TeacherCoursesPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../redux/store";
 import "./TeacherCoursesPage.scss";
