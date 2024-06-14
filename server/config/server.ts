b348775ab306ad9cd75762b315040868e46d081c
GayathriArundheepak <127670381+GayathriArundheepak@users.noreply.github.com>
2024-06-14 11:28:22 +0530

    server core option updated 1

--- server/config/server.ts
+++ server/config/server.ts
@@ -10,25 +10,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
      // Use cookie-parser middleware
       app.use(cookieParser());
-
-      const corsOptions = {
-          origin: 'https://studdybuddy-learning.netlify.app',
-          methods: 'GET, POST, OPTIONS',
-          allowedHeaders: 'Authorization, Content-Type',
-          preflightContinue: false,
-          optionsSuccessStatus: 204
-        };
-        
-        app.use(cors(corsOptions));
       
      // Enable CORS middleware
-// app.use(cors({
+app.use(cors({
     
-//      origin: 'http://localhost:3000', // Allow requests from this origin
+     origin: 'https://studdybuddy-learning.netlify.app', // Allow requests from this origin
      
-//     credentials: true // Allow credentials (cookies)
-//   })
-//   );
+    credentials: true // Allow credentials (cookies)
+  })
+  );
     return app
 }
 export default createServer;
